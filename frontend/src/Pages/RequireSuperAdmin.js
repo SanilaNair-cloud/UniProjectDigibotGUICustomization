946ff88aca3b46d1d014714789dbd2544b7b354c
usqamrit <u1159083@umail.usq.edu.au>
2025-04-30 22:01:45 +1000

    Work in progress changes from main

--- frontend/src/Pages/RequireSuperAdmin.js
+++ frontend/src/Pages/RequireSuperAdmin.js
@@ -1,12 +1,30 @@
+/**
+ * RequireSuperAdmin – Route Guard for Superadmin Access Only
+ *
+ * This component protects certain routes in the app so only users
+ * with the "superadmin" role can access them.
+ *
+ * If a non-superadmin user tries to access the route, they will be
+ * redirected to the /company-portal page.
+ *
+ * Usage:
+ * <RequireSuperAdmin>
+ *   <ProtectedComponent />
+ * </RequireSuperAdmin>
+ */
+
 import { Navigate } from "react-router-dom";
 
 const RequireSuperAdmin = ({ children }) => {
+  // Get user info from localStorage
   const userData = JSON.parse(localStorage.getItem("USER_INFO"));
 
+  // Redirect if user is not a superadmin or not logged in
   if (!userData || userData.user_type !== "superadmin") {
     return <Navigate to="/company-portal" />;
   }
 
+  // If superadmin, allow access to the children components
   return children;
 };
 
